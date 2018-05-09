4011fad50190c43b1f37651c265bbe9d0e2c574d
devroopb <indroneil20@gmail.com>
2018-05-09 18:23:32 +0530

    initial commit

---
+++ a2/celebsStudents-IfOracleUVICused.sql
@@ -0,0 +1,7 @@
+INSERT INTO Albums SELECT * FROM c##thomo.Albums;
+INSERT INTO Celebs SELECT * FROM c##thomo.Celebs;
+INSERT INTO Enemies SELECT * FROM c##thomo.Enemies;
+INSERT INTO MovieTitles SELECT * FROM c##thomo.MovieTitles;
+INSERT INTO Relationships SELECT * FROM c##thomo.Relationships;
+INSERT INTO Releases SELECT * FROM c##thomo.Releases; 
+INSERT INTO StarredIn SELECT * FROM c##thomo.StarredIn;
